c1c74e6e591c8acafc9f769735d3e0cc192ebc78
viniciusomar <vinicius.omar@prolins.com.br>
2022-09-27 20:48:02 -0300

    feat: Makes front-end login functional.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+    selector: 'app-nav',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
 
-  constructor() { }
+export class NavComponent implements OnInit
+{
+    loginModel:any = {};
+    loggedIn: boolean = false;
 
-  ngOnInit(): void {
-  }
+    constructor(public accountService: AccountService) { }
 
+    ngOnInit(): void
+    {
+    }
+
+    login()
+    {
+        this.accountService.login(this.loginModel).subscribe(
+        {
+            next: response =>
+            {
+                this.loggedIn = true;
+                console.log(response);
+            },
+            error: error =>
+            {
+                this.loggedIn = false;
+                console.log("Error message: " + error.error);
+                console.log(error);
+            }
+        })
+    }
+
+    logout()
+    {
+       this.loggedIn = false;
+    }
 }
