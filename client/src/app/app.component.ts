30191d7c2c3b6a2176153b8795aa117ed0e9d8fc
viniciusomar <vinicius.omar@prolins.com.br>
2022-09-27 20:54:33 -0300

    feat: Makes 'Login' persistent. Stores on local storage.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
 	selector: 'app-root',
@@ -11,11 +13,12 @@ export class AppComponent implements OnInit{
 	title = 'The dating app';
 	users: any;
 
-	constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient, private accountService: AccountService) {}
 
 	ngOnInit()
 	{
 		this.getUsers();
+        this.setCurrentUser();
 	}
 
 	getUsers()
@@ -26,5 +29,11 @@ export class AppComponent implements OnInit{
                 error: error => console.log(error)
             }
 		)
+    }
+
+    setCurrentUser()
+    {
+        const user: User = JSON.parse(localStorage.getItem('user'));
+        this.accountService.setCurrentUser(user);
     }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -39,5 +39,6 @@ export class NavComponent implements OnInit
     logout()
     {
        this.loggedIn = false;
+        this.accountService.logout();
     }
 }
